12e1ac76c9c8a7658f910b5cfcb0bfc6002c1a89
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-09-27 14:44:00 -0700

    feat: add channel joining and notification functionality in chat application

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,25 +1,24 @@
-module.exports = (io, socket, onlineUsers) => {
+module.exports = (io, socket, onlineUsers, channels) => {
   socket.on('get online users', () => {
     // Send current online users to the requesting client
     socket.emit('get online users', onlineUsers);
   });
 
   // Listen for "new user" events from clients
-  socket.on('new user', (data) => {
-    console.log(`✋ ${data.username} has joined the chat! ✋`);
-    
-    // Store username in socket for later use
-    socket.username = data.username;
-    
-    // Add user to online users tracking
-    onlineUsers[data.username] = socket.id;
-    
-    // Send current online users list to the new user
-    socket.emit('online users', { users: Object.keys(onlineUsers) });
-    
-    // Broadcast to ALL clients that someone joined
-    io.emit("new user", { username: data.username });
+  socket.on('new channel', (newChannel) => {
+    // Create new channel with empty message array
+    channels[newChannel] = [];
+    // Have the creator join the channel room
+    socket.join(newChannel);
+    // Tell all clients about new channel
+    io.emit('new channel', newChannel);
+    // Switch the creator to the new channel
+    socket.emit('user changed channel', {
+      channel: newChannel,
+      messages: channels[newChannel]
+    });
   });
+};
 
   // Handle disconnect
   socket.on('disconnect', () => {
