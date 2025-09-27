9e266e6e693c2837fb43ddc8a314b9b7297fd500
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-09-27 14:07:20 -0700

    add: implement user creation functionality with socket.io integration

--- public/index.js
+++ public/index.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
-  // Connect to the socket.io server
   const socket = io.connect();
+
+  $('#create-user-btn').click((e) => {
+    e.preventDefault();
+    let username = $('#username-input').val();
+    if(username.length > 0) {
+      // Emit custom "new user" event to server
+      socket.emit('new user', username);
+      $('.username-form').remove(); // Hide form after joining
+    }
+  });
 });
